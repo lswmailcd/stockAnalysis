c53bf78a6983c0381adf219f85ba7f2eb916705e
lswmailcd <lswmailcd@163.com>
2019-03-08 14:13:03 +0800

    add fund invest and step invest

--- fundStepInvestCompute.py
+++ fundStepInvestCompute.py
@@ -8,12 +8,13 @@ import tushare as ts
 import time
 import stockTools as sT
 
-STARTYEAR = 2019   #投资起始年
+STARTYEAR = 2018   #投资起始年
 STARTMONTH = 1 #投资起始月份
-buyDay = 1      #投资起始日期
+startDay = 1      #投资起始日期
 ENDYEAR = 2019  #投资结束年
 ENDMONTH = 3  #投资结束月份
-saleDay = 3  #投资结束日
+endDay = 3  #投资结束日
+buyDay = 20  #定投日
 
 print u"WARNING:请注意基金历史分红情况，默认以现金分红为准！"
 str = raw_input("默认现金分红进行计算请按'回车',如需以红利再投进行计算请按'c',退出请按'q': ")
@@ -21,10 +22,10 @@ if str=="q" : exit(0)
 stype = "1" #现金分红
 if str=="c" :
     stype = "2" #红利再投
-print u"计算时间段为：",STARTYEAR,u"年",STARTMONTH,u"月", buyDay,u"日\
----",ENDYEAR,u"年",ENDMONTH,u"月", saleDay,u"日"
+print u"计算时间段为：",STARTYEAR,u"年",STARTMONTH,u"月", startDay,u"日\
+---",ENDYEAR,u"年",ENDMONTH,u"月", endDay,u"日"
 startDate = sT.getDateString(STARTYEAR, STARTMONTH, buyDay)
-saleDate = sT.getDateString(ENDYEAR, ENDMONTH, saleDay)
+saleDate = sT.getDateString(ENDYEAR, ENDMONTH, endDay)
 data = xlrd.open_workbook('.\\data\\etfdata.xls')
 table = data.sheets()[0]
 nrows = table.nrows-1
@@ -46,23 +47,23 @@ worksheet = workbook.add_sheet('dataResult')
 for i in range(count):
     foundData = 0
     if code[i] == u'' : continue
-    url = "http://fundex.eastmoney.com/FundWebServices/FundSylCalculator.aspx?fc=" + code[i]
-    url = url + "&stime=" + startDate + "&etime=" + saleDate
-    url = url + "&stype=" + stype + "&sgfl=" + "0" + "&shfl=" + "0" + "&sg=" + "10000" + "&lx=1"
+    url = "http://fund.eastmoney.com/data/FundInvestCaculator_AIPDatas.aspx?fcode=" + code[i]
+    url = url + "&sdate=" + startDate + "&edate" + saleDate + "&shdate" + saleDate
+    url = url + "&round" + "1" + "&dtr" + "%s" %(buyDay) + "&p=" + "0" + "&je=" + "10000"
+    url = url + "&stype=" + stype + "&needfirst=" + "2" + "&jsoncallback=FundDTSY.result"
     data = urllib.urlopen(url).read()
     time.sleep(1)
-    infoStr = data.split(':')
+    infoStr = data.split('|')
     if infoStr[0]=='var Result={"error"':
         print data
         continue
-    rateList = data.split(":")[4].split(",")[0].split('"')
-    rate = float(rateList[1])/100.0
+    rate = float(infoStr[6][:-1])/100.0
     worksheet.write(i, 0, code[i])
     worksheet.write(i, 1, name[i])
     worksheet.write(i, 2, rate)
     worksheet.write(i, 3, startDate)
     worksheet.write(i, 4, saleDate)
-    print code[i],name[i], "%.2f%%" %(rate*100.0)
+    print code[i], name[i], "%.2f%%" % (rate * 100.0)
 
 workbook.save('.\\data\\dataResult.xls')
 print "Invest result has been wrotten to dataResult.xls"
