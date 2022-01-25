ad76432aec15fb201cb53466251774638ab4f93d
lswmailcd <lswmailcd@163.com>
2022-01-25 15:06:50 +0800

    Update stockReportReader.py

--- stockReportReader.py
+++ stockReportReader.py
@@ -1,13 +1,13 @@
-#coding:utf-8
-
-from sqlalchemy import create_engine
 import numpy as np
 import urllib
 import bs4
-import time
 import xlrd
-import xlwt
 import stockTools as sT
+import time
+from random import randint
+
+startdate = time.strftime('%Y-%m-%d',time.localtime(time.time()-24*60*60*90*2))#公告起始时间为三个月前
+endate = time.strftime('%Y-%m-%d',time.localtime(time.time()))
 
 data = xlrd.open_workbook('.\\data\\StockList.xls')
 table = data.sheets()[0]
@@ -15,35 +15,38 @@ nrows = table.nrows-1
 a = np.zeros([nrows])
 code = np.array(a, dtype=np.unicode)
 name = np.array(a, dtype=np.unicode)
-count  = 0
+count = 0
 for i in range(nrows):
     if table.cell(i + 1, 0).value!="":
         code[count] = table.cell(i + 1, 0).value
-        name[count] = sT.getStockNameByCode(code[count]).decode('utf8')
+        name[count] = sT.getStockNameByCode(code[count])
+        sname, yearToMarket,_,_ = sT.getStockBasics(code[count])
+        if yearToMarket == 0:
+            print( code[count], name[count], u"上市时间不详!")
+            exit(1)
         count += 1
-
-engine = create_engine('mysql://root:0609@127.0.0.1:3306/stockdatabase?charset=utf8', encoding='utf-8')
-conn = engine.connect()
+            
 for i in range(count):
-    sqlString = "select timetomarket from stockbasics_20180126 "
-    sqlString += "where code="
-    sqlString += code[i]
-    ret = conn.execute(sqlString)
-    result = ret.first()
-    if result == None:
-        print  code[i], name[i] ,"股票基本数据获取失败！"
-        continue
-    elif result.timetomarket == 0:
-        print  code[i], name[i],"股票上市时间数据为空！"
-        continue
-    yearToMarket = int(result.timetomarket / 1E4)
-    if yearToMarket>STARTYEAR:
-        print code[i], name[i],"股票上市时间比起始查询时间晚！"
-        continue
-    print code[i], name[i], yearToMarket,"年上市！"
+    time.sleep(randint(1,3))
+    url = "https://vip.stock.finance.sina.com.cn/corp/go.php/vCB_AllBulletin/stockid/"
+    url += code[i]
+    url += ".phtml"
+    response = urllib.request.urlopen(url=url)
+    # response = urllib.urlopen(request)
+    data = response.read().decode("gbk")
+    bs = bs4.BeautifulSoup(data, "lxml")
+    table = bs.find_all('table',{'class':'table2'})[1]
+    div = table.find('div',{'class':'datelist'})
+    tlist = div.get_text().split()
+    noticeList=[]
+    for j in range(len(tlist)-1):
+        if j<len(tlist)-2:
+            noticeList.append((tlist[j][-10:], tlist[j+1][:-10]))
+        else:
+            noticeList.append((tlist[j][-10:], tlist[j+1]))
 
-url = "http://vip.stock.finance.sina.com.cn/corp/go.php/vISSUE_ShareBonus/stockid/"
-url += code[i]
-url += ".phtml"
-data = urllib.urlopen(url).read()
-bs = bs4.BeautifulSoup(data, "lxml")
+    for d, c in noticeList:
+        if startdate<=d<=endate and "回购" in c:
+            print(d,c)
+    print()
+
