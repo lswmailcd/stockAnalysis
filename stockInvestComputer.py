9ff9366e286ec10ba5cb9ec921a3598faf1196be
lswmailcd <lswmailcd@163.com>
2022-04-15 17:11:02 +0800

    Update stockInvestComputer.py

--- stockInvestComputer.py
+++ stockInvestComputer.py
@@ -11,7 +11,7 @@ import time
 style_percent = xlwt.easyxf(num_format_str='0.00%')
 style_finance = xlwt.easyxf(num_format_str='￥#,##0.00')
 
-startdate = time.strftime('%Y-%m-%d',time.localtime(time.time()-24*60*60*90))#起始时间为6个月前
+startdate = time.strftime('%Y-%m-%d',time.localtime(time.time()-24*60*60*180))#起始时间为6个月前
 STARTYEAR, STARTMONTH, STARTDAY = sT.splitDateString(startdate)
 
 enddate = time.strftime('%Y-%m-%d', time.localtime(time.time())) #统计结束时间为当前时间
@@ -22,7 +22,6 @@ table = data.sheets()[0]
 nrows = table.nrows-1
 code = []
 name = []
-price = []
 count = 0
 for i in range(nrows):
     if table.cell(i + 1, 0).value!="" or table.cell(i + 1, 1).value!="":
@@ -35,7 +34,6 @@ for i in range(nrows):
             if name[i] == None:
                 code[i] = ""
                 continue
-            price.append(table.cell(i + 1, 2).value)
             sname, yearToMarket,_ ,_ = sT.getStockBasics(code[i])
             if yearToMarket == 0:
                 print( code[i], name[i], u"上市时间不详!")
@@ -45,20 +43,26 @@ for i in range(nrows):
                                sT.getDateString(ENDYEAR, ENDMONTH, ENDDAY))
 
 print()
+stockList=[]
 for i in range(count):
     if code[i] == "" or code[i]=='0.0': continue
     # 查找创新低前的最低点
     priceList=[]
     date = startdate
-
+    pLow, dLow=10000.0, startdate
     while date <= enddate:
         bFound, closePrice, actDate = sT.getClosePriceBackward(code[i], date)
-        if bFound: priceList.append((closePrice, actDate))
+        if bFound and  closePrice<pLow:
+            pLow = closePrice
+            dLow = actDate
         date = sT.getDateString(*sT.createCalender().getNextWorkday(date))
-    recentPrice, recentDate = priceList[-1]
-    priceList.sort()
-    print("{} {}，股票价格最低价{:.2f}元, 日期{}, 当前价{:.2f}元, 日期{}, 最低价距当前日期{}天".\
-        format(code[i], name[i], priceList[0][0], priceList[0][1],recentPrice, recentDate, sT.createCalender().dayDiff(priceList[0][1],recentDate) ))
+    recentPrice, recentDate = closePrice, actDate
+    stockList.append((code[i], name[i], pLow, dLow, recentPrice, recentDate, sT.createCalender().dayDiff(dLow,recentDate)))
+
+stockList.sort(reverse=True, key=lambda x:x[-1])
+for code, name, pLow, dLow, pCur, dCur, diff in stockList:
+    print("{} {}，股票价格最低价{:.2f}元, 日期{}, 当前价{:.2f}元, 日期{}, 最低价距当前日期{}天\n".\
+        format(code, name, pLow, dLow, pCur, dCur, diff))
 
 
 
